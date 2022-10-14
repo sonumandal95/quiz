a5ffc68d927b10502eb741310fa1592e632ee674
sonumandal95 <55188699+sonumandal95@users.noreply.github.com>
2022-10-14 23:45:23 +0530

    Rules dialog

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -17,7 +17,7 @@ const Home = () => {
     <div style={{ 'textAlign': 'center' }}>
       <h2>St. Robert's High School, Parsudih</h2>
       <Grid><img width={250} height={250} src="schoolLogo.png" /></Grid>
-      <h3>Welcome to Science Quiz</h3>
+      <h2>Welcome to Science Quiz</h2>
       <Container>
         <Button variant="contained" onClick={handleNextClick}>Next</Button>
       </Container>
--- src/views/round/index.js
+++ src/views/round/index.js
@@ -1,31 +1,102 @@
-import React from 'react'
-import { Button, Grid, Card, CardContent } from '@mui/material';
+import React, { useState } from 'react'
+import {
+  Button,
+  Grid,
+  IconButton,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Typography
+} from '@mui/material';
 import styled from '@emotion/styled'
+import Info from '@mui/icons-material/Info'
 
 const Round = () => {
+  const [openRuleDialog, setOpenRuleDialog] = useState(false)
+  const [rules, setRules] = useState("");
   const rounds = ["round1", "round2", "round3", "round4"]
   const Container = styled.div(props => ({
     display: 'flex',
     flexDirection: props.column && 'column',
-    marginRight: '20px'
+  }))
+
+  const Box = styled.div(props => ({
+    display: 'flex',
+    flexDirection: props.column && 'column',
+    borderWidth: '1px',
+    borderColor: '#dbdbdb',
+    borderRadius: '5px',
+    borderStyle: 'solid',
+    padding: '15px',
+    marginBottom: '20px'
   }))
 
   const handleSubjectClick = (round) => {
     window.open(`/numbers/${round.toLowerCase()}`, "_self")
   }
 
+  const handleOpenRule1 = () => {
+    const data = "Rule 1"
+    setRules(data);
+    setOpenRuleDialog(true);
+  }
+
+  const handleOpenRule2 = () => {
+    const data = "Rule2"
+    setRules(data);
+    setOpenRuleDialog(true);
+  }
+
   return (
     <div style={{ 'textAlign': 'center' }}>
       <h2>Rounds</h2>
-      <Card variant="outlined" style={{ 'paddingTop': '10'}}>
-        <Grid container style={{ 'justifyContent': 'center' }}>
-          {rounds.map((round, index) =>
-            <Container key={round}>
-              <Button variant="contained" onClick={() => handleSubjectClick(round)}>Round {index + 1}</Button>
-            </Container>
-          )}
+      <Box>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid container style={{ justifyContent: 'center', marginBottom: 20 }}>
+            <IconButton color="primary" aria-label="rules" component="label" onClick={handleOpenRule1}>
+              <Info />
+            </IconButton>
+          </Grid>
+          <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }} style={{ justifyContent: 'center' }}>
+            {rounds.map((round, index) =>
+              <Container key={round}>
+                <Button variant="contained" onClick={() => handleSubjectClick(round)}>Round {index + 1}</Button>
+              </Container>
+            )}
+          </Grid>
         </Grid>
-      </Card>
+      </Box>
+      <Box>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid container style={{ justifyContent: 'center', marginBottom: 20 }}>
+            <IconButton color="primary" aria-label="rules" component="label" onClick={handleOpenRule2}>
+              <Info />
+            </IconButton>
+          </Grid>
+          <Grid container rowSpacing={1} style={{ 'justifyContent': 'center' }}>
+            <Container>
+              <Button variant="contained">Round 5</Button>
+            </Container>
+          </Grid>
+        </Grid>
+      </Box>
+      <Dialog open={openRuleDialog} onClose={() => setOpenRuleDialog(!openRuleDialog)} >
+        <DialogTitle>Rules</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <Typography>
+              {rules}
+            </Typography>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" color="primary" size="small" onClick={() => setOpenRuleDialog(!openRuleDialog)}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 };
